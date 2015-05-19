25ae3e4ddb67516f848efbb8f2d7037552a736e0
M4nuski <the_man994@hotmail.com>
2015-05-18 20:46:58 -0400

    Commit with STAGE ALL instead of just one file...

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Dec2Hex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(155, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 39);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "00";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 57);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(671, 266);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(298, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(385, 39);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "00000000";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 336);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Dec - Hex - Bin Converter / E.C. 2015";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Dec2Hex
+{
+    public partial class Form1 : Form
+    {
+
+        private bool suppressed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox3.AppendText(textBox1.Text + " -> 0x" + textBox2.Text + " b'" + textBox4.Text + "'\n");
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private static int safeIntParse(string s)
+        {
+            int result;
+            int.TryParse(s, NumberStyles.Integer, NumberFormatInfo.CurrentInfo, out result);
+            return result;
+        }
+
+        private static int safeHexParse(string s)
+        {
+            int result;
+            int.TryParse(s, NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out result);
+            return result;
+        }
+
+        private static int safeBinParse(string s)
+        {
+            int result;
+            try
+            {
+                result = Convert.ToInt32(s, 2);
+            }
+            catch (Exception)
+            {
+                result = 0;
+            }
+            return result;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!suppressed) ParseAll(textBox1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!suppressed) ParseAll(textBox2);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!suppressed) ParseAll(textBox4);
+        }
+
+        private void ParseAll(TextBox origin)
+        {
+            suppressed = true;
+            if (origin == textBox1)
+            {
+                var val = safeIntParse(textBox1.Text);
+
+                if (val > 65535)
+                {
+                    textBox2.Text = val.ToString("X8");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(32, '0');
+                }
+                else if (val > 255)
+                {
+                    textBox2.Text = val.ToString("X4");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(16, '0');
+                }
+                else
+                {
+                    textBox2.Text = val.ToString("X2");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(8, '0');
+                }
+            }
+            else if (origin == textBox2)
+            {
+                var val = safeHexParse(textBox2.Text);
+
+                if (val > 65535)
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(32, '0');
+                }
+                else if (val > 255)
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(16, '0');
+                }
+                else
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox4.Text = Convert.ToString(val, 2).PadLeft(8, '0');
+                }
+
+            }
+            else if (origin == textBox4)
+            {
+                var val = safeBinParse(textBox4.Text);
+
+                if (val > 65535)
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox2.Text = val.ToString("X8");
+                }
+                else if (val > 255)
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox2.Text = val.ToString("X4");
+                }
+                else
+                {
+                    textBox1.Text = val.ToString("D");
+                    textBox2.Text = val.ToString("X2");
+                }
+
+            }
+            suppressed = false;
+        }
+    }
+}
+
+
